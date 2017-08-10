3af68ca0362b68d5b23e11fd15b71323d7b15b2a
on4r <Puqpetmaster@gmail.com>
2017-08-10 10:54:14 +0200

    add vim plugins init script

---
+++ vim/.vim/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# clones vim plugin repos
+#
+
+DIR="bundle"
+declare -a arr=("https://github.com/itchyny/lightline.vim.git"
+                "https://github.com/scrooloose/nerdtree.git"
+                "https://github.com/myusuf3/numbers.vim.git")
+
+eval "mkdir -p ${DIR} && cd ${DIR}"
+
+for i in "${arr[@]}"
+do
+  eval "git clone ${i}"
+done
