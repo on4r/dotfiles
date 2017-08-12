0c3929e5c10ecba537946c09086f17247e276418
Thomas Vogl <Puqpetmaster@gmail.com>
2017-08-12 18:18:19 +0200

    set target dir to use relative path

--- vim/.vim/init.sh
+++ vim/.vim/init.sh
@@ -3,15 +3,16 @@
 # clones vim plugin repos
 #
 
-DIR="bundle"
-declare -a arr=("https://github.com/itchyny/lightline.vim.git"
-                "https://github.com/scrooloose/nerdtree.git"
-                "https://github.com/myusuf3/numbers.vim.git"
-                "https://github.com/morhetz/gruvbox.git")
+target_dir="$(dirname $0)/bundle"
+declare -a plugins=("https://github.com/itchyny/lightline.vim.git"
+                    "https://github.com/scrooloose/nerdtree.git"
+                    "https://github.com/myusuf3/numbers.vim.git"
+                    "https://github.com/morhetz/gruvbox.git")
 
-eval "mkdir -p ${DIR} && cd ${DIR}"
+eval "mkdir -p ${target_dir} && cd ${target_dir}"
+echo "█▓░ cloning vim plugins to ${target_dir}"
 
-for i in "${arr[@]}"
+for i in "${plugins[@]}"
 do
   eval "git clone ${i}"
 done
