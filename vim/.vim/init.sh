62f5ccbbe61808be2f2f4db445eeb81221a8d295
Thomas Vogl <Puqpetmaster@gmail.com>
2017-08-12 15:19:44 +0200

    load gruvbox via pathogen and remove unused themes

--- vim/.vim/init.sh
+++ vim/.vim/init.sh
@@ -6,7 +6,8 @@
 DIR="bundle"
 declare -a arr=("https://github.com/itchyny/lightline.vim.git"
                 "https://github.com/scrooloose/nerdtree.git"
-                "https://github.com/myusuf3/numbers.vim.git")
+                "https://github.com/myusuf3/numbers.vim.git"
+                "https://github.com/morhetz/gruvbox.git")
 
 eval "mkdir -p ${DIR} && cd ${DIR}"
 
